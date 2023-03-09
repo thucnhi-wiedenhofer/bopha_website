0d9809a1e4e8dea5efc9e0f04b7e26223933f78a
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2023-03-09 10:02:44 +0100

    modif button footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,51 +1,43 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "./../public/images/logo_bopha.png";
 import arrow from "./../public/images/icons/arrow-icon.svg";
 
-
-
 export default function Footer() {
   return (
     <footer id="footer">
-  <div className="footer-img">
-    <Image src={logo} alt="logo" width="100" height="100" />
-  </div>
-  <div className="container">
-    <nav className="navbar">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" href="/">
-            Accueil
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href="/admin">
-            Administration
-          </Link>
-        </li>
+      <div className="footer-img">
+        <Image src={logo} alt="logo" width="100" height="100" />
+      </div>
+      <div className="container">
+        <nav className="navbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" href="/">
+                Accueil
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/admin">
+                Administration
+              </Link>
+            </li>
 
-        <li className="nav-item">
-          <Link className="nav-link" href="/contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <Link target="_blank" href="/galerie">
-      <button
-        type="button"
-        className="button btn btn-dark"
-      >
-        Voir la Galerie
-        <Image src={arrow} alt="flèche" className="arrow" />
-      </button>
-    </Link>
-  </div>
-</footer>
-
-  )
+            <li className="nav-item">
+              <Link className="nav-link" href="/galerie">
+                Galerie
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Link target="_blank" href="/contact">
+          <button type="button" className="button btn btn-dark">
+            Contactez-moi
+            <Image src={arrow} alt="flèche" className="arrow" />
+          </button>
+        </Link>
+      </div>
+    </footer>
+  );
 }
-
-
